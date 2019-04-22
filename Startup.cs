8ad5f83b4ab2e69f2c52b0739c08ecbdd7746a6b
Sergey Ezhov <sezhov@modem.by>
2019-04-22 13:47:29 +0300

    initial commit

---
+++ Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using LightInject;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace WebApplication1
+{
+
+    public class Service
+    {
+        private static int ConstuructorCallsCount;
+
+        public Service()
+        {
+            Debug.WriteLine("Service constructor called");
+            Interlocked.Increment(ref ConstuructorCallsCount);
+
+            Thread.Sleep(5000);
+        }
+
+        public int Count => ConstuructorCallsCount;
+    }
+
+        
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                .AddControllersAsServices()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSingleton<Service>();
+        }
+        public void ConfigureContainer(IServiceContainer container)
+        {
+
+        }
+
+            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
---
+++ TestController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/test")]
+    public class TestController
+    {
+        private readonly IServiceProvider serviceProvider;
+
+        public TestController(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        [HttpGet]
+        public int Get()
+        {
+            var t1 = Task.Run(() => serviceProvider.GetService<Service>());
+            var t2 = Task.Run(() => serviceProvider.GetService<Service>());
+            Task.WaitAll(t1, t2);
+
+
+            return t1.Result.Count;
+        }
+
+    }
+}
